4d3cee662abacb3db7aa65296f804f4242d7896f
James Dunstan <jdunstan355@gmail.com>
2024-06-13 12:40:44 +1000

    Practising useStates

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Colorizer from './Colorizer'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+   <div className='App'>
+    <Colorizer />
+   </div>
+  )
+}
+
+export default App
